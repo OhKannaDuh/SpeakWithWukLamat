3692c45df654ef7213bba09214d02e0b0e2ac276
OhKannaDuh <uimokd@gmail.com>
2025-09-22 13:29:47 +0100

    alpha for eyes on

--- SpeakWithWukLamat/Patches/ARealmReborn/MSQ/PrudenceAtThisJunctionQuestPatch.cs
+++ SpeakWithWukLamat/Patches/ARealmReborn/MSQ/PrudenceAtThisJunctionQuestPatch.cs
@@ -4,13 +4,16 @@ using SpeakWithWukLamat.Data.Quests.Solution;
 
 namespace SpeakWithWukLamat.Patches.ARealmReborn.MSQ;
 
-[QuestPatch("Faye, Doug", "7.31")]
+[QuestPatch("Faye", "7.31")]
 public class PrudenceAtThisJunctionQuestPatch(ILogger logger) : IQuestPatch
 {
-    // public QuestId Id { get; } = new(3852);
-    public QuestId Id { get; } = new(533);
+    public QuestId Id { get; } = new(3852);
 
     public void Patch(QuestData data, QuestSolution solution)
     {
+        logger.Debug("This was just made to test the patching system for now.");
+        // What is needed for this quest?
+        // Need to equip items of at least ilvl 5 in the 5 main slots
+        // This should just mean ensuring we buy a hat from the nearby merchant and equipping it
     }
 }
